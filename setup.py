d6e4f39de5778be34e39c52d86de90b13a453ba9
Raza Aziz <razaaziz9191@gmail.com>
2025-03-20 21:13:01 +0500

    Folder Structure Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="RAG-based Project",
+    version='0.0.0',
+    author="Raza Aziz",
+    author_email="razaaziz9191@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
